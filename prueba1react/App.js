423e172c73c06d1f9ba69f71e554d7532420d613
deyberst <deyberst@gmail.com>
2021-04-09 21:11:08 -0500

    Cambio de color
    fondo  a azul

--- prueba1react/App.js
+++ prueba1react/App.js
@@ -36,7 +36,7 @@ const styles = StyleSheet.create ({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
+    backgroundColor: 'cyan',
   },
   title: {fontSize: 30, color: '#ffff'},
 
