b6e3604df060b035a24c9c7533c6e15229a963e7
juanferr98 <j98fer@gmail.com>
2021-08-13 15:23:56 -0500

    Holi
    a

--- newtonapp/lib/pages/index_page.dart
+++ newtonapp/lib/pages/index_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
