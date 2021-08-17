4d2124b7a0c72407890df76215e315cba4b4d1f6
deyberst <deyberst@gmail.com>
2021-08-17 11:27:18 -0500

    Revert "Organizar Vistas"
    This reverts commit 418ea194fc3de48d6feaf734d12524cfb0912165.

--- newtonapp/lib/pages/index_page.dart
+++ newtonapp/lib/pages/index_page.dart
@@ -6,7 +6,7 @@ class IndexPage extends StatelessWidget {
   const IndexPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('La Newton App Re-Loco'),
@@ -16,56 +16,48 @@ class IndexPage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          textoInicial(),
-          botonAprendizaje(),
-          botonRetos(),
+          Container(
+            //Texto inicial en la App
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            child: const Text('Elija a donde quere ir'),
+          ),          
+          Container(// Boton para ir a la zona de Aprendizaje
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.center,            
+            child: SignInButtonBuilder(
+                //Aqui esta las config del Boton
+                icon: Icons.verified_user,
+                backgroundColor: Colors.orange,
+                text: 'Aprendizaje',
+                onPressed: () {
+                  /*Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SignInPage()));*/
+                },
+                ),
+                
+          ),
+          Container(//Boton para ir a los Retos de la App
+              padding: const EdgeInsets.all(10),
+              alignment: Alignment.center,
+              child: SignInButtonBuilder(
+                  //Aqui esta las config del Boton
+                  icon: Icons.person_add,
+                backgroundColor: Colors.indigo,
+                text: 'Retos',
+                  onPressed: () {
+                    /*Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => RegisterPage()));*/
+                  },
+                  )
+          ),
         ],
       ),
-      backgroundColor: Colors.purple.shade200, //Color de Fondo
+      backgroundColor: Colors.purple.shade200,//Color de Fondo
     );
-  }
-
-  Widget textoInicial() {
-    return Container(
-      //Texto inicial en la App
-      padding: const EdgeInsets.all(10),
-      alignment: Alignment.center,
-      child: const Text('Elija a donde quere ir'),
-    );
-  }
-
-  Widget botonAprendizaje() {
-    return Container(
-      // Boton para ir a la zona de Aprendizaje
-      padding: const EdgeInsets.all(10),
-      alignment: Alignment.center,
-      child: SignInButtonBuilder(
-        //Aqui esta las config del Boton
-        icon: Icons.verified_user,
-        backgroundColor: Colors.orange,
-        text: 'Aprendizaje',
-        onPressed: () {
-          /*Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => SignInPage()));*/
-        },
-      ),
-    );
-  }
-
-  Widget botonRetos() {
-    return Container(
-        //Boton para ir a los Retos de la App
-        padding: const EdgeInsets.all(10),
-        alignment: Alignment.center,
-        child: SignInButtonBuilder(
-          //Aqui esta las config del Boton
-          icon: Icons.person_add,
-          backgroundColor: Colors.indigo,
-          text: 'Retos',
-          onPressed: () {
-            /*Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => RegisterPage()));*/
-          },
-        ));
   }
 }
+
+/*Widget _TextoInicial(){
+
+}*/
