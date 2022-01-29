96f0890326576d66ceda7501c04ef02ec086beb6
deyberst <deyberst@gmail.com>
2022-01-28 19:18:18 -0500

    Index terminado

--- newtonapp/lib/pages/index.dart
+++ newtonapp/lib/pages/index.dart
@@ -33,87 +33,30 @@ class Index extends StatelessWidget {
                       bottomRight: Radius.circular(90)),
                 ),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                  
-                     Image.asset('../assets/images/logo.png',width: 450,) ,
-                 
-          ]
-        
-                ),
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.asset(
+                        '../assets/images/logo.png',
+                        width: 450,
+                      ),
+                    ]),
               ),
-              
-              //color: Colors.black,
 
+              //color: Colors.black,
             ),
             Container(
-                height: MediaQuery.of(context).size.height * 0.6,
-                color: Colors.black,
-                    child:      Column(
-              
+              height: MediaQuery.of(context).size.height * 0.6,
+              //color: Colors.black,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  //titulo(context),
+                  botones(context),
+                ],
+              ),
             ),
-            ),
-
           ],
         ),
-        /*
-          decoration: BoxDecoration(
-              gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-            Color.fromRGBO(200, 107, 250, 1),
-            Color.fromRGBO(123, 44, 191, 1),
-          ])),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(
-                height: 80,
-              ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  //aqui va el logo
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Expanded(
-                  child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(60),
-                            topRight: Radius.circular(60)),
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.all(20),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          //crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            titulo(context),
-                            botones(context),
-                          ],
-                        ),
-                      )))
-            ],
-          )),
-*/
-        /* anterior
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          //crossAxisAlignment: CrossAxisAlignment.center,
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            titulo(context),
-            botones(context),
-          ],
-        ),
-      ),
-      */
       ),
     );
   }
@@ -149,6 +92,7 @@ class Index extends StatelessWidget {
       width: double.infinity,
       height: MediaQuery.of(context).size.height * .5,
       padding: const EdgeInsets.all(30),
+
       child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
@@ -167,23 +111,26 @@ class Index extends StatelessWidget {
         child: MaterialButton(
           minWidth: 250.0,
           height: 80.0,
-          color: const Color.fromRGBO(255, 170, 0, 1), //naranja
+          color: const Color.fromRGBO(255, 123, 0, 1), //naranja
           //color: const Color.fromRGBO( 7, 194, 184, 1), turquesa antiguo
           //color: Colors.white,
+
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(5.0),
+            borderRadius: BorderRadius.circular(10.0),
           ),
+
           onPressed: () {
             Navigator.of(context).pushNamed('login');
             //Navigator.of(context).pushNamed('index');
           },
+     
           child: const Text(
-            'Login',
+            'Entrar',
             style: TextStyle(
               fontFamily: 'QBold',
               color: Colors.white,
               //color: Color.fromRGBO( 145, 99, 203, 1),
-              fontSize: 35.0,
+              fontSize: 25.0,
               fontWeight: FontWeight.bold,
             ),
           ),
@@ -197,20 +144,21 @@ class Index extends StatelessWidget {
         child: MaterialButton(
           minWidth: 250.0,
           height: 80.0,
-          color: Colors.white,
+          
+          color: const Color.fromRGBO(255, 123, 0, 1),
           //color: const Color.fromRGBO( 0, 180, 216, 1),
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(5.0),
+            borderRadius: BorderRadius.circular(10.0),
           ),
           onPressed: () {
             Navigator.of(context).pushNamed('register');
           },
           child: const Text(
-            'Registro',
+            'Registrarse',
             style: TextStyle(
               fontFamily: 'QBold',
-              color: const Color.fromRGBO(145, 99, 203, 1),
-              fontSize: 35.0,
+              color: Colors.white,
+              fontSize: 25.0,
               fontWeight: FontWeight.bold,
             ),
           ),
