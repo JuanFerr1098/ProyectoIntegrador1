2fff9c0c02e49053313036be71be754c3669d67c
JuanFerr98 <36459186+1214741@users.noreply.github.com>
2022-03-02 22:04:21 -0600

    reduzco codigo de index

--- newtonapp/lib/pages/index.dart
+++ newtonapp/lib/pages/index.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-//import 'package:flutter/src/material/colors.dart';
 
 class Index extends StatelessWidget {
   const Index({Key? key}) : super(key: key);
@@ -10,7 +9,7 @@ class Index extends StatelessWidget {
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
     return Scaffold(
-      // backgroundColor: const Color.fromRGBO(123,44, 191, 1),//Fondo de la pantalla
+      //backgroundColor: const Color.fromRGBO(123,44, 191, 1),//Fondo de la pantalla
       //backgroundColor: const Color.fromRGBO( 145, 99, 203, 1),//Fondo de la pantalla
       body: cuerpo(context),
     );
@@ -44,22 +43,25 @@ class Index extends StatelessWidget {
     return SafeArea(
         child: ListView(
       children: <Widget>[
-        Container(
-          height: 300,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-              Color.fromRGBO(200, 107, 250, 1),
-              Color.fromRGBO(123, 44, 191, 1),
-            ]),
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(90),
-                bottomRight: Radius.circular(90)),
-          ),
-          child: const Image(image: AssetImage('assets/images/logo.png')),
-        ),
+        logo(),
         botones(context),
       ],
     ));
+  }
+
+  Widget logo() {
+    return Container(
+      height: 300,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+          Color.fromRGBO(200, 107, 250, 1),
+          Color.fromRGBO(123, 44, 191, 1),
+        ]),
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(90), bottomRight: Radius.circular(90)),
+      ),
+      child: const Image(image: AssetImage('assets/images/logo.png')),
+    );
   }
 
   Widget botones(context) {
@@ -68,75 +70,41 @@ class Index extends StatelessWidget {
       width: double.infinity,
       height: MediaQuery.of(context).size.height * .5,
       padding: const EdgeInsets.all(30),
-
       child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          //crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            botonlogin(context),
-            botonRegistro(context),
+            boton(context, 'login', 'Entrar'),
+            boton(context, 'register', 'Registrarse')
           ]),
     );
   }
 
-  Widget botonlogin(context) {
+  Widget boton(context, String ruta, String texto) {
     return Container(
-        padding: const EdgeInsets.all(10),
-        alignment: Alignment.center,
-        child: MaterialButton(
+      padding: const EdgeInsets.all(10),
+      alignment: Alignment.center,
+      child: MaterialButton(
           minWidth: 250.0,
           height: 80.0,
-          color: const Color.fromRGBO(255, 123, 0, 1), //naranja
-          //color: const Color.fromRGBO( 7, 194, 184, 1), turquesa antiguo
-          //color: Colors.white,
-
+          color: const Color.fromRGBO(255, 123, 0, 1),
+          //color: const Color.fromRGBO( 0, 180, 216, 1),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10.0),
           ),
-
           onPressed: () {
-            Navigator.of(context).pushNamed('login');
-            //Navigator.of(context).pushNamed('index');
+            Navigator.of(context).pushNamed(ruta);
           },
-
-          child: const Text(
-            'Entrar',
-            style: TextStyle(
+          child: Text(
+            texto,
+            style: const TextStyle(
               fontFamily: 'QBold',
               color: Colors.white,
               //color: Color.fromRGBO( 145, 99, 203, 1),
               fontSize: 25.0,
               fontWeight: FontWeight.bold,
             ),
-          ),
-        ));
-  }
-
-  Widget botonRegistro(context) {
-    return Container(
-        padding: const EdgeInsets.all(10),
-        alignment: Alignment.center,
-        child: MaterialButton(
-            minWidth: 250.0,
-            height: 80.0,
-            color: const Color.fromRGBO(255, 123, 0, 1),
-            //color: const Color.fromRGBO( 0, 180, 216, 1),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            onPressed: () {
-              Navigator.of(context).pushNamed('register');
-            },
-            child: const Text(
-              'Registrarse',
-              style: TextStyle(
-                fontFamily: 'QBold',
-                color: Colors.white,
-                //color: Color.fromRGBO( 145, 99, 203, 1),
-                fontSize: 25.0,
-                fontWeight: FontWeight.bold,
-              ),
-            )));
+          )),
+    );
   }
 }
